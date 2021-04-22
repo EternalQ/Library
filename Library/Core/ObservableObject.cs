1bd71ffc337dbdff7a7c1269eacc0a4fa7ba2d3f
Данила Птуха <dptuha1406@gmail.com>
2021-04-23 02:19:57 +0300

    Updated pages, core VM, color resources

--- Library/Core/ObservableObject.cs
+++ Library/Core/ObservableObject.cs
@@ -1,16 +1,52 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace Library.Core
 {
-    class ObservableObject : INotifyPropertyChanged
+    class ObservableObject : INotifyPropertyChanged, INotifyDataErrorInfo
     {
+        public bool HasErrors => _propertyErrors.Any();
+
         public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        private readonly Dictionary<string, List<string>> _propertyErrors = new Dictionary<string, List<string>>();
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            return (_propertyErrors.ContainsKey(propertyName)) ? _propertyErrors[propertyName] : null;
+        }
+
+        public virtual void AddError(string propertyName, string errorMessage)
+        {
+            if (!_propertyErrors.ContainsKey(propertyName))
+            {
+                _propertyErrors.Add(propertyName, new List<string>());
+            }
+            _propertyErrors[propertyName].Add(errorMessage);
+            OnErrorsChanged(propertyName);
+        }
+
+        public virtual void ClearErrors([CallerMemberName] string propertyName = null)
+        {
+            if (_propertyErrors.Remove(propertyName))
+            {
+                OnErrorsChanged(propertyName);
+            }
+        }
 
         protected void OnPropertyChanged([CallerMemberName] string name = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        protected void OnErrorsChanged([CallerMemberName] string propertyName = null)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
         }
     }
 }
